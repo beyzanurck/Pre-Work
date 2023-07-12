9689e6262256d41dca3827056ddb05b5c55e43c8
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-07-12 11:53:33 -0500

    solved q5

--- Week 5/7_FunctionsChallenges.js
+++ Week 5/7_FunctionsChallenges.js
@@ -102,6 +102,21 @@ console.log(removeFromString('Hello School', 6, 400)) // --> 'Hello '
 // indexOf(arr2, 2); // --> 1
 // let arr3 = [1, 2];
 // indexOf(arr3, 10); // --> -1
+function indexOf(arr, num){
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index]==num){
+            return index
+        }        
+    }
+    return -1
+}
+let arr = [5, 10, 15, 20];
+console.log(indexOf(arr, 20)); // --> 3
+let arr2 = [1, 2, 3, 4, 5];
+console.log(indexOf(arr2, 2)); // --> 1
+let arr3 = [1, 2];
+console.log(indexOf(arr3, 10)); // --> -1
+
 
 // Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
 // The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
